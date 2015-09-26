132eb0839a0fbe46d65875b3052f0f6749f2f1d1
jonathanchiu <jonathanchiu7@gmail.com>
2015-09-26 16:06:25 -0400

    Queue

--- data-structures/queue.py
+++ data-structures/queue.py
@@ -17,7 +17,7 @@ class Queue:
       raise QueueError("Can't enqueue to a full queue")
 
     self.data.append(e)
-    return self.data  
+    return self.data
 
   def dequeue(self):
     if self.is_empty():
@@ -32,4 +32,27 @@ class Queue:
 
   def is_full(self):
     return len(self.data) == self.max_size
+
+  def peek(self):
+    if self.is_empty():
+      raise QueueError("Can't peek at an element of an empty queue")
+    else:
+      return self.data[0]
+
+
+test = Queue(5)
+print test.is_empty() == True
+test.enqueue(6)
+test.enqueue(99)
+print test.peek() == 6
+print test.dequeue() == 6
+print test.dequeue() == 99
+test.enqueue(812)
+test.enqueue(82)
+test.enqueue(38)
+test.enqueue(91)
+print test.is_full() == False
+test.enqueue(999)
+print test.is_full() == True
+
 
